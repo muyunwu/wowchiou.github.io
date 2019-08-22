99667930f441a35f4b1b3606898e35c3c5329f99
wowchiou <wowchiou@gmail.com>
2019-08-22 17:28:19 +0800

    Updates

---
+++ precache-manifest.07ee616f9b65b3f0a20f9e705f60a02a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2643784d5ac90078897a77d16bd01b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43266746045fb5b73545",
+    "url": "/static/css/main.43428bfc.chunk.css"
+  },
+  {
+    "revision": "7f9e8c83922dc3a084f1",
+    "url": "/static/js/2.541c8705.chunk.js"
+  },
+  {
+    "revision": "43266746045fb5b73545",
+    "url": "/static/js/main.ab4164c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
